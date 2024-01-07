00ea04942b109c86cf28006babf592f9bea5664c
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-01-07 16:03:59 +0500

    updated Navbar
    Added login and sign up button but not added their logic yet.

---
+++ components/Login.js
@@ -0,0 +1,52 @@
+// pages/login.js
+import { Box, Button, FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here, you can add your login logic
+        // For example, you can validate the credentials and authenticate the user
+
+        // For demo purposes, let's assume authentication is successful
+        // Redirect to a dashboard or home page after login
+        router.push("/dashboard");
+    };
+
+    return (
+        <Box maxW="md" mx="auto" mt="10">
+            <VStack spacing="4" p="6" borderWidth="1px" borderRadius="md" boxShadow="md">
+                <form onSubmit={handleSubmit}>
+                    <FormControl id="email" isRequired>
+                        <FormLabel>Email address</FormLabel>
+                        <Input
+                            type="email"
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl id="password" isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <Button type="submit" colorScheme="purple" mt="4" w="full">
+                        Login
+                    </Button>
+                </form>
+            </VStack>
+        </Box>
+    );
+};
+
+export default Login;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React, { useState } from "react";
 import {
     Menu,
     MenuButton,
@@ -8,47 +9,73 @@ import {
     Box,
     Spacer,
     MenuItem,
+    Button,
 } from "@chakra-ui/react";
 import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
 import { BsSearch } from "react-icons/bs";
 import { FiKey } from "react-icons/fi";
 
-// Navbar
-const Navbar = () => (
-    <Flex p="2" borderBottom="1px" borderColor="gray.100">
-        {/* Logo */}
-        <Box fontSize="3xl" color="purple.600" fontWeight="bold">
-            <Link href="/" paddingLeft="2">
-                Property-Pulse
-            </Link>
-        </Box>
-        <Spacer />
-        <Box>
-            {/* Menu */}
-            <Menu>
-                <MenuButton
-                    as={IconButton}
-                    icon={<FcMenu />}
-                    variant="outlined"
-                    color="red.400"
-                />
-                <MenuList>
-                    <Link href="/" passHref>
-                        <MenuItem icon={<FcHome />}>Home</MenuItem>
-                    </Link>
-                    <Link href="/search" passHref>
-                        <MenuItem icon={<BsSearch />}>Search</MenuItem>
-                    </Link>
-                    <Link href="/search?purpose=for-sale" passHref>
-                        <MenuItem icon={<FcAbout />}>Buy Property</MenuItem>
-                    </Link>
-                    <Link href="/search?purpose=for-rent" passHref>
-                        <MenuItem icon={<FiKey />}>Rent Property</MenuItem>
-                    </Link>
-                </MenuList>
-            </Menu>
-        </Box>
-    </Flex>
-);
+const Navbar = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <Flex p="2" borderBottom="1px" borderColor="gray.100">
+            {/* Logo */}
+            <Box fontSize="3xl" color="purple.600" fontWeight="bold">
+                <Link href="/" paddingLeft="2">
+                    Property-Pulse
+                </Link>
+            </Box>
+            <Spacer />
+            <Box>
+                {/* Menu */}
+                <Menu>
+                    <MenuButton
+                        as={IconButton}
+                        icon={<FcMenu />}
+                        variant="outlined"
+                        color="red.400"
+                    />
+                    <MenuList>
+                        <Link href="/" passHref>
+                            <MenuItem icon={<FcHome />}>Home</MenuItem>
+                        </Link>
+                        <Link href="/search" passHref>
+                            <MenuItem icon={<BsSearch />}>Search</MenuItem>
+                        </Link>
+                        <Link href="/search?purpose=for-sale" passHref>
+                            <MenuItem icon={<FcAbout />}>Buy Property</MenuItem>
+                        </Link>
+                        <Link href="/search?purpose=for-rent" passHref>
+                            <MenuItem icon={<FiKey />}>Rent Property</MenuItem>
+                        </Link>
+                    </MenuList>
+                </Menu>
+            </Box>
+            {/* Conditional rendering of Login and Sign Up Buttons */}
+            <Box>
+                {!isLoggedIn && (
+                    <>
+                        <Button onClick={handleLogin} mr="1">
+                            Login
+                        </Button>
+                        <Button colorScheme="purple" onClick={handleLogin}>
+                            Sign Up
+                        </Button>
+                    </>
+                )}
+            </Box>
+        </Flex>
+    );
+};
 
 export default Navbar;
