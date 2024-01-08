ff9e04aabb6d04591dfb0c1b4c7e1692df70fcba
Khursheed Ahmed <118465990+khursheedahmedd@users.noreply.github.com>
2024-01-08 23:39:08 +0500

    Update Property.jsx

--- components/Property.jsx
+++ components/Property.jsx
@@ -50,7 +50,7 @@ const Property = ({
                         </Box>
                         {/* Price */}
                         <Text fontWeight="bold" fontSize="lg">
-                            PKR {millify(price)}
+                            AED {millify(price)}
                             {rentFrequency && `/${rentFrequency}`}
                         </Text>
                     </Flex>
